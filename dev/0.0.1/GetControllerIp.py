5156919b250eaef6053a93b78a0803e543396324
am2308 <b110020510@gmail.com>
2020-10-04 17:01:24 +0530

    Update GetControllerIp.py

--- dev/0.0.1/GetControllerIp.py
+++ dev/0.0.1/GetControllerIp.py
@@ -7,7 +7,7 @@ Region = str(sys.argv[1])
 #This will create a client of aws ec2 for the specified region
 ec2 = boto3.client('ec2', region_name=Region)
 #InstanceList = ['etcd-0', 'controller-0', 'worker-0', 'worker-1', 'worker-2']
-
+data = ""
 #Above created client will interect with ec2 instances using AWS SDKs based upon filtered instances
 response = ec2.describe_instances()
 
@@ -20,7 +20,9 @@ for reservation in response["Reservations"]:
         for tags in instance['Tags']:
                 if tags['Key'] == 'Name' and tags['Value'].startswith('cont') and instance['State']['Name'] == "running":
                         if tags['Value'].startswith('cont'):
-                                data = "[controller]\ncontroller ansible_ssh_host=" + instance['PublicIpAddress'] + "\n"
+                                data = data + "[controller]\ncontroller ansible_ssh_host=" + instance['PublicIpAddress'] + "\n"
+                elif tags['Key'] == 'Name' and tags['Value'] == "worker-0" and instance['State']['Name'] == "running":
+                        data = data + "[worker]\nworker ansible_ssh_host=" + instance['PublicIpAddress'] + "\n"
 
 
 
